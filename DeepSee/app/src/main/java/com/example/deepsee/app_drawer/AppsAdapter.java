4ff56cc90e43d332fb8523ede4c5eee5288833c8
fazam0616 <fazam0616@hotmail.com>
2024-02-28 21:10:35 -0500

    Added very basic implementation of app displaying and running
    (cherry picked from commit 18e82bbcf812e079892f01639132e8461d433436)

---
+++ DeepSee/app/src/main/java/com/example/deepsee/app_drawer/AppsAdapter.java
@@ -0,0 +1,82 @@
+package com.example.deepsee.app_drawer;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.app.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.deepsee.R;
+
+import java.util.List;
+
+public class AppsAdapter extends RecyclerView.Adapter<AppsAdapter.ViewHolder>{
+    private List<PackageInfo> apps;
+    private PackageManager pm;
+
+    public AppsAdapter(List<PackageInfo> apps, PackageManager pm) {
+        this.apps = apps;
+        this.pm = pm;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View appSquare = inflater.inflate(R.layout.app_square, parent, false);
+        ViewHolder v = new ViewHolder(appSquare);
+        v.con = context;
+        return v;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AppsAdapter.ViewHolder holder, int position) {
+        PackageInfo p = apps.get(position);
+        try {
+            holder.icon.setImageDrawable(pm.getApplicationIcon(p.packageName));
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+
+        holder.icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent launchIntent = new Intent(pm.getLaunchIntentForPackage(p.packageName));
+                holder.con.startActivity(launchIntent);
+            }
+        });
+
+        try {
+            holder.name.setText(pm.getApplicationLabel(pm.getApplicationInfo(p.packageName,0)));
+        } catch (PackageManager.NameNotFoundException e) {
+            holder.name.setText("Oops");
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return apps.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ImageView icon;
+        public TextView name;
+        public Context con;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            icon = (ImageView) itemView.findViewById(R.id.app_icon);
+            name = (TextView) itemView.findViewById(R.id.app_name);
+        }
+    }
+}
