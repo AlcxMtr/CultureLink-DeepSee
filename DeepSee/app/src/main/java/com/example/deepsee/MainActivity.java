87dccde4f5883db6070ece97680f47ce858da3b7
Angel Chen <angelchen396@gmail.com>
2024-03-13 14:02:02 -0400

    emergency pages in progress

--- DeepSee/app/src/main/java/com/example/deepsee/MainActivity.java
+++ DeepSee/app/src/main/java/com/example/deepsee/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.deepsee;
+import android.Manifest;
 
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +20,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import android.provider.ContactsContract;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,11 +30,57 @@ public class MainActivity extends AppCompatActivity {
     public HashMap<Integer, List<ApplicationInfo>> categories;
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission is granted
+                String[] projection = new String[]{
+                        ContactsContract.Profile._ID,
+                        ContactsContract.Profile.DISPLAY_NAME_PRIMARY,
+                        ContactsContract.Profile.LOOKUP_KEY,
+                        ContactsContract.Profile.HAS_PHONE_NUMBER
+                };
+                Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, projection, null, null, null);
+                if (cursor.moveToFirst()) {
+                    do {
+                        int id = cursor.getColumnIndex(ContactsContract.Contacts._ID);
+                        int displayName = cursor.getColumnIndex(ContactsContract.Profile.DISPLAY_NAME);
+
+                        if(id >= 0 && displayName >= 0){
+                            String contactID = cursor.getString(id);
+                            Cursor phoneNumCursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                                    null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=?", new String[] { contactID }, null);
+
+                            if (phoneNumCursor.moveToFirst()){
+                                int num = phoneNumCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                                if(num >=0){
+                                    System.out.println(cursor.getString(displayName) + ": " + phoneNumCursor.getString(num));
+                                }
+                            }
+                            phoneNumCursor.close();
+
+
+                        }
+
+
+                    } while (cursor.moveToNext());
+                }
+                cursor.close();
+            } else {
+                System.out.println("Woops...");
+            }
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button showHideButton = findViewById(R.id.show_hide_button);
+        Button emergencyButton = findViewById(R.id.emergency_button);
         final PackageManager pm = getPackageManager();
 
         // Get Package List:
@@ -64,6 +114,17 @@ public class MainActivity extends AppCompatActivity {
                 transaction.commit();
             }
         });
+        emergencyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new EmergencyContactFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, fragment);
+                transaction.commit();
+            }
+        });
+
+        requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, 1);
     }
 
     // Toggles the App Drawer:
