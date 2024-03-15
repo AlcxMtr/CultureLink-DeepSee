e04a7a6be7583816aacbc8db8b633caf7c8101e3
aks-456 <akshath.sohal@gmail.com>
2024-03-15 14:04:06 -0400

    User can change text size
    User is now properly able to change text size for all components in the app -- unless an element from an app specifically ignores system settings.

--- DeepSee/app/src/main/java/com/example/deepsee/SettingsActivity.java
+++ DeepSee/app/src/main/java/com/example/deepsee/SettingsActivity.java
@@ -1,29 +1,104 @@
 package com.example.deepsee;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.app.PendingIntent.getActivity;
 
+import android.app.FragmentManager;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toolbar;
+import android.widget.Toast;
 
-import com.example.deepsee.R;
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    ListView lv;
+
+    ListView lvSettings;
+//    AlertDialog.Builder builder;
+
+    int textSize =  -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        getSupportActionBar().setTitle("Deepsee Settings");
+//        getSupportActionBar().setTitle("Deepsee Settings");
 
-        lv = (ListView) findViewById(R.id.lvSettings);
+        lvSettings = (ListView) findViewById(R.id.lvSettings);
 
+        ArrayAdapter<String> array_adapter = getStringArrayAdapter();
+        lvSettings.setAdapter(array_adapter);
+
+        lvSettings.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i == 1) {
+
+                    boolean settingsPer = Settings.System.canWrite(SettingsActivity.this);
+                    if(settingsPer){
+                        //Log.d("ListView", "Item clicked: " + i);
+                        String[] options = {"Small", "Medium", "Large"};
+                        AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+                        builder.setTitle("Set Text Size");
+                        builder.setItems(options, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int size) {
+                                if (size == 0) {
+
+                                    //SettingsActivity.this.setTheme((R.style.FontSizeSmall));
+                                    Settings.System.putFloat(getBaseContext().getContentResolver(),
+                                            Settings.System.FONT_SCALE, (float) 0.5);
+
+
+                                } else if(size == 1) {
+
+                                    //SettingsActivity.this.setTheme((R.style.FontSizeMedium));
+                                    Settings.System.putFloat(getBaseContext().getContentResolver(),
+                                            Settings.System.FONT_SCALE, (float) 1.0);
+
+                                } else if(size == 2) {
+
+                                    //SettingsActivity.this.setTheme((R.style.FontSizeLarge));
+                                    Settings.System.putFloat(getBaseContext().getContentResolver(),
+                                            Settings.System.FONT_SCALE, (float) 2.0);
+                                }
+                            }
+                        });
+                        builder.show();
+
+                    }else{
+                        Toast.makeText(SettingsActivity.this, "Please allow write permissions", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                        intent.setData(Uri.parse("package:" + SettingsActivity.this.getPackageName()));
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+
+                    }
+                }
+            }
+        });
+
+
+    }
+
+    @NonNull
+    private ArrayAdapter<String> getStringArrayAdapter() {
         ArrayList<String> setting_arr = new ArrayList<String>();
         setting_arr.add("Easy Viewing");
         setting_arr.add("Display Settings");
@@ -36,6 +111,6 @@ public class SettingsActivity extends AppCompatActivity {
 
 
         ArrayAdapter<String> array_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, setting_arr);
-        lv.setAdapter(array_adapter);
+        return array_adapter;
     }
 }
