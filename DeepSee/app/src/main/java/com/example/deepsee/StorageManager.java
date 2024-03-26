8af3d5a5dce475ed5570c80565a1ff822c008f6f
Alex Motor <alexandermotormail@gmail.com>
2024-03-26 14:14:37 -0400

    Context receiving fixed in storage manager

--- DeepSee/app/src/main/java/com/example/deepsee/StorageManager.java
+++ DeepSee/app/src/main/java/com/example/deepsee/StorageManager.java
@@ -9,21 +9,22 @@ import java.util.List;
 import java.io.*;
 import java.util.HashMap;
 import java.util.List;
-
-
 // This class handles the storage, reading, and management of app-local files.
 // Use it to get or set data structures and effectively memoize them for use later.
 // Ideally one instance of this is created at a given time.
 // The get and set functions can be used without the object but ensure
 public class StorageManager {
-    public Matrices matrices;   // For Alex. Replace "Matrices" with the object you use tostore stuff.
+    //public Matrices matrices;   // For Alex. Replace "Matrices" with the object you use to store stuff.
     public HashMap categories;
     public List<ApplicationInfo> apps;      // For Fahim
 
-    public StorageManager() {
-        this.matrices = null;
+    private Context context;
+
+    public StorageManager(Context context) {
+        //this.matrices = null;
         this.categories = null;
         this.apps = null;
+        this.context = context;
     }
 
     // APPS LIST FOR APP DRAWER:
@@ -47,21 +48,20 @@ public class StorageManager {
     }
 
     // MATRICES:
-    private Matrices getMatrices() {
-        this.matrices = readObjectFromFile("matrices.dat");
-        return this.matrices;
-    }
+//    private Matrices getMatrices() {
+//        this.matrices = readObjectFromFile("matrices.dat");
+//        return this.matrices;
+//    }
 
-    private void setMatrices(Matrices matrices) {
-        writeObjectToFile("matrices.dat", matrices);
-    }
+//    private void setMatrices(Matrices matrices) {
+//        writeObjectToFile("matrices.dat", matrices);
+//    }
 
     // General reader and writer helper functions. They return null if the read or write failed
     // for any reason.
     private <T> T readObjectFromFile(String filename) {
-        Context context = contextGetter.getAppContext().getApplicationContext();    // BAD FIX. DO LATER.
-        try (FileInputStream fis = context.openFileInput(filename);
-             ObjectInputStream ois = new ObjectInputStream(fis)) {
+        try (FileInputStream fis = this.context.openFileInput(filename);
+            ObjectInputStream ois = new ObjectInputStream(fis)) {
             // bad bad unchecked cast:
             return (T) ois.readObject();
         } catch (Exception e) {
@@ -72,8 +72,7 @@ public class StorageManager {
     }
 
     private <T> void writeObjectToFile(String filename, T object) {
-        Context context = contextGetter.getAppContext().getApplicationContext();    // BAD FIX. DO LATER.
-        try (FileOutputStream fos = context.openFileOutput(filename, Context.MODE_PRIVATE);
+        try (FileOutputStream fos = this.context.openFileOutput(filename, Context.MODE_PRIVATE);
              ObjectOutputStream oos = new ObjectOutputStream(fos)) {
             oos.writeObject(object);
         } catch (Exception e) {
