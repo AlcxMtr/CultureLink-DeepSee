6eaf3cf0412e39cf6e656086d5dae3eadc4ba534
Muhannad Al-Jamali <muhannad.aljamali@mail.utoronto.ca>
2024-03-29 03:16:09 -0400

    Fixed merge bugs

--- DeepSee/app/src/main/java/com/example/deepsee/MainActivity.java
+++ DeepSee/app/src/main/java/com/example/deepsee/MainActivity.java
@@ -15,7 +15,6 @@ import android.os.Bundle;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
-import com.example.deepsee.accessibility.TextAndSpeech;
 import com.example.deepsee.databinding.ActivityMainBinding;
 import com.example.deepsee.weather.WeatherListener;
 import com.example.deepsee.weather.WeatherRequest;
@@ -23,8 +22,6 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-
-import com.example.deepsee.accessibility.TextAndSpeech;
 import com.example.deepsee.databinding.ActivityMainBinding;
 
 
